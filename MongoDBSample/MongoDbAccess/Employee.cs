6e084ae46af1a515b2546432b602c07f69e0ba52
mohitjain02 <mohit.jain02@nagarro.com>
2018-01-28 21:49:38 +0530

    Crud Operation using Mongo+Visual Studio

---
+++ MongoDBSample/MongoDbAccess/Employee.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MongoDbAccess
+{
+   public class Employee
+    {
+        public ObjectId Id { get; set; }
+        public string EmployeeName { get; set; }
+
+        public int EmpId { get; set; }
+
+        public int Age { get; set; }
+    }
+}
---
+++ MongoDBSample/MongoDbAccess/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace MongoDbAccess
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new MongoClient("mongodb://localhost:27017");
+            var empDb = client.GetDatabase("Employee");
+            var collection = empDb.GetCollection<Employee>("employeeInformation");
+            var empId = new ObjectId("5a6dbf91e431a2be34fe8ade");
+            var result = collection.Find(x => x.Id == empId).SortBy(y => y.Age).Limit(5).ToListAsync().Result;
+
+            foreach (var emp in result)
+            {
+                Console.WriteLine($"EmployeeName : { emp.EmployeeName} ");
+            }
+            ////var document = new Employee()
+            ////{
+            ////    Age = 12,
+            ////    EmployeeName = "mohit",
+            ////    EmpId = 3146563
+            ////};
+
+            ////collection.InsertOneAsync(document);
+
+            Console.Read();
+
+        }
+
+        public static void GetEmployeeInformation()
+        {
+
+        }
+    }
+}
